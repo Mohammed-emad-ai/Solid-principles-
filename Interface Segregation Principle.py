4bf1badd9ebf7df1554aa9da6cc0776551366ea6
Mohammed emad <mohammedemad110200@gmail.com>
2024-12-23 08:45:46 +0200

    Update Interface Segregation Principle.py

--- Interface Segregation Principle.py
+++ Interface Segregation Principle.py
@@ -1,18 +1,15 @@
 from abc import ABC, abstractmethod
 
-# Interface for Printable
 class Printable(ABC):
     @abstractmethod
     def print_info(self):
         pass
 
-# Interface for Calculable
 class Calculable(ABC):
     @abstractmethod
     def calculate(self):
         pass
 
-# Class that implements both interfaces
 class Invoice(Printable, Calculable):
     def __init__(self, item, price):
         self.item = item
@@ -22,9 +19,8 @@ class Invoice(Printable, Calculable):
         print(f"Item: {self.item}, Price: {self.price}")
     
     def calculate(self):
-        return self.price * 1.1  # Adding a 10% tax
+        return self.price 
 
-# Usage
 invoice = Invoice("Laptop", 1000)
-invoice.print_info()  # Output: Item: Laptop, Price: 1000
-print(f"Total with tax: {invoice.calculate()}")
+invoice.print_info() 
+print(f"Total : {invoice.calculate()}")
